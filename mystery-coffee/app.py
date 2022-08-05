8d5b73a92bb4afc0152eef2a2583f75c5ab44de1
Boris Kondev <boriskondev@gmail.com>
2022-08-05 16:50:19 +0300

    Some important tweaks added, ready for testing

--- mystery-coffee/app.py
+++ mystery-coffee/app.py
@@ -1,36 +1,42 @@
+from datetime import date
 import pandas as pd
 import random
 import json
 import os
 
-# TBD:
+# ---------------------------------------------------------------------------------------
 # What happens when the monthly emails are odd?
-# Pairs to be saved in separate file and the results to be formatted
 # When testing different iterations at some point the script freezes - investigate why
 # To calculate when an infinite loop will begin and what the condition to escape it to be
+# ---------------------------------------------------------------------------------------
 
 mystery_coffee_db_file = "mystery_coffee_db.json"
+current_month_db_file = "db.xlsx"
+
+current_month_records = pd.read_excel(current_month_db_file, sheet_name=0, skiprows=None)
+current_month_emails = current_month_records["Email"].tolist()
+
+current_month, current_year = str(date.today().month), date.today().year
+current_month = "0" + current_month if len(current_month) == 1 else current_month
+
+current_month_pairs_file = f"mystery coffee pairs_{current_month}_{current_year}.txt"
 
 if os.path.exists(mystery_coffee_db_file):
-    with open(mystery_coffee_db_file) as json_file:
-        mystery_coffee_db = json.load(json_file)
+    with open(mystery_coffee_db_file) as f:
+        mystery_coffee_db = json.load(f)
 else:
     mystery_coffee_db = {}
 
-current_month_db = "db.xlsx"
-current_month_records = pd.read_excel(current_month_db, sheet_name=0, skiprows=None)
-current_month_emails = current_month_records["Email"].tolist()
-
 pairs = []
+# iterations = 1
 
 while current_month_emails:
     pair = []
+
     first_email = random.choice(current_month_emails)
 
     if first_email not in mystery_coffee_db:
         mystery_coffee_db[first_email] = []
-
-    pair.append(first_email)
 
     second_email = random.choice(current_month_emails)
 
@@ -38,9 +44,11 @@ while current_month_emails:
         # print(f"in WHILE with e1: {first_email} and e2: {second_email}.")
         # print(f"{len(current_month_emails)} pairs left")
         second_email = random.choice(current_month_emails)
+        # print("Iteration: " + str(iterations))
+        # iterations += 1
 
     # print("out")
-    pair.append(second_email)
+    pair += [first_email, second_email]
     pairs.append(pair)
 
     mystery_coffee_db[first_email].append(second_email)
@@ -50,17 +58,16 @@ while current_month_emails:
 
     mystery_coffee_db[second_email].append(first_email)
 
-    # print(f"{len(pairs)} pairs awarded")
-
     current_month_emails.remove(first_email)
     current_month_emails.remove(second_email)
 
-for i in range(len(pairs)):
-    print(f"Pair {i+1}: {pairs[i][0]} and {pairs[i][1]}")
+    # print("Iteration: " + str(iterations))
+    # iterations += 1
 
-current_month_pairs = f"Current pairs.txt"
+if os.path.exists(current_month_pairs_file):
+    os.remove(current_month_pairs_file)
 
-with open(current_month_pairs, "a+") as file:
+with open(current_month_pairs_file, "a+") as file:
     for i in range(len(pairs)):
         file.write(f"Pair {i + 1}: {pairs[i][0]} and {pairs[i][1]}\n")
 
