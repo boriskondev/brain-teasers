075297c9f14d5e9c61ccd61c108ffdf45ff44e0e
Boris Kondev <boriskondev@gmail.com>
2022-12-02 17:09:41 +0200

    Added HTML email body

--- mystery-coffee/send_emails.py
+++ mystery-coffee/send_emails.py
@@ -7,10 +7,10 @@ olApp = win32.Dispatch("Outlook.Application")
 olNS = olApp.GetNameSpace("MAPI")
 
 # File name removed for safe reasons
-pairs_file = ""
+pairs_file = "test.txt"
 pairs = []
 
-i = 1
+# i = 1
 
 with open(pairs_file) as f:
     [pairs.append(line.strip()) for line in f.readlines()]
@@ -21,14 +21,23 @@ for pair in pairs:
     emails = emails.split(" + ")
 
     # Construct the email item object
-    mailItem = olApp.CreateItem(0)
-    mailItem.Subject = f"Mystery Coffee pair {i}"
-    mailItem.BodyFormat = 1
-    mailItem.Body = f"Hello from the other side {i}"
-    mailItem.To = ";".join(emails)
-    mailItem.Display()
+    email = olApp.CreateItem(0)
+    email.Subject = f"Congrats_Mystery Coffee Buddy"
+    # email.Subject = f"Mystery Coffee pair {i}"
+    email.BodyFormat = 1
+    # email.Body = f"Hello from the other side {i}"
+    email.HTMLBody = """
+    <p>Good day to both of you 🌞</p>
+    <p>Thank you for taking part in our Mystery Coffee.</p>
+    <p>I am so excited to let you know that you two were randomly selected to have a “Coffee meeting” 🥐☕/🍟🍻.</p>
+    <p>You could meet whenever you’d like during this month and talk about whatever you want to. </p>
+    <p>Enjoy, have fun and taka a picture 😝</p>
+    <p>If you need anything, we are here for you!  </p>
+    """
+    email.To = ";".join(emails)
+    email.Display()
 
-    mailItem.Save()
-    mailItem.Send()
+    email.Save()
+    email.Send()
 
-    i += 1
+    # i += 1
